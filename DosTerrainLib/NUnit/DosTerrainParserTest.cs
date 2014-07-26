f204dfc626f6dbe6b4e252c44bb5d96a19017c0b
celludriel <de_smedt_kenny@telenet.be>
2014-07-26 09:06:51 +0200

    Added extra read test 100 by 100 test fails because it seems the DOS editor doesn't create the expected amount of triangles (to be investigated)

--- DosTerrainLib/NUnit/DosTerrainParserTest.cs
+++ DosTerrainLib/NUnit/DosTerrainParserTest.cs
@@ -25,10 +25,9 @@ namespace DosTerrainLib.NUnit
             Assert.AreEqual(1, terrain.Layers.Count);
             LayerType layer = terrain.Layers.ElementAt(0);
             Assert.AreEqual(999, layer.Index);
-            UInt32 triangleSizeInBytes = 50 * 50 * 12;
+            UInt32 triangleSizeInBytes = 50 * 50 * 24;
             Assert.AreEqual(triangleSizeInBytes, layer.Triangles.Length * 12);
-            UInt32 intensitiesSizeInBytes = 50 * 50 * 4;
-            Assert.AreEqual(intensitiesSizeInBytes, layer.Intensities.Length * 4);
+            Assert.AreEqual(0, layer.Intensities.Length * 4);
         }
 
         [Test]
@@ -45,11 +44,42 @@ namespace DosTerrainLib.NUnit
             Assert.AreEqual(1, terrain.Layers.Count);
             LayerType layer = terrain.Layers.ElementAt(0);
             Assert.AreEqual(999, layer.Index);
-            UInt32 triangleSizeInBytes = 32 * 32 * 12;
+            UInt32 triangleSizeInBytes = 32 * 32 * 24;
             Assert.AreEqual(triangleSizeInBytes, layer.Triangles.Length * 12);
-            UInt32 intensitiesSizeInBytes = 32 * 32 * 4;
-            Assert.AreEqual(intensitiesSizeInBytes, layer.Intensities.Length * 4);
+            Assert.AreEqual(0, layer.Intensities.Length * 4);
         }
 
+        [Test]
+        public void parser64By64ExtraLayersTest()
+        {
+            String pathToTestFile = "C:\\Git\\Repos\\DOS_Terrain_Lib\\DosTerrainLib\\TestData\\Terrain_002.data";
+            DosTerrainParser parser = new DosTerrainParser();
+            DosTerrain terrain = parser.ReadDosTerrain(64, 64, pathToTestFile);
+
+            UInt32 heightMapSizeInBytes = 1089 * 4;
+            Assert.AreEqual(heightMapSizeInBytes, terrain.HeightMapSize);
+            Assert.AreEqual(heightMapSizeInBytes, terrain.HeightMapData.Length * 4);
+
+            Assert.AreEqual(3, terrain.Layers.Count);
+            UInt32 triangleSizeInBytes = 32 * 32 * 24;
+            
+            LayerType layer = terrain.Layers.ElementAt(0);
+            Assert.AreEqual(999, layer.Index);
+            Assert.AreEqual(triangleSizeInBytes, layer.Triangles.Length * 12);
+            Assert.AreEqual(0, layer.Intensities.Length * 4);
+
+            triangleSizeInBytes = 32 * 32 * 24;
+            UInt32 intensitiesInBytes = 32 * 32 * 4;
+            
+            layer = terrain.Layers.ElementAt(1);
+            Assert.AreEqual(0, layer.Index);
+            Assert.AreEqual(triangleSizeInBytes, layer.Triangles.Length * 12);
+            Assert.AreEqual(intensitiesInBytes, layer.Intensities.Length * 4);
+
+            layer = terrain.Layers.ElementAt(2);
+            Assert.AreEqual(1, layer.Index);
+            Assert.AreEqual(triangleSizeInBytes, layer.Triangles.Length * 12);
+            Assert.AreEqual(intensitiesInBytes, layer.Intensities.Length * 4);
+        }
     }
 }
